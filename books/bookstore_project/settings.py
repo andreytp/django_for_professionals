914bd79e49b0da21828ae75e30ae430ad79ec802
andreytp <andreytp@gmail.com>
2021-04-22 23:32:53 +0300

    set up dockerfile composefile settings

--- books/bookstore_project/settings.py
+++ books/bookstore_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-l&riw^1s(fa*725l7a@&x!hyt09vz9jqqvrq4hbjl1jn#tln2h
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*',]
 
 
 # Application definition
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'bookstore_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'db',
+        'PORT': '5432',
     }
 }
 
