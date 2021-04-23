301a7de017e8176c52e7674a11bd2c042cdb95f4
andreytp <andreytp@gmail.com>
2021-04-23 23:43:16 +0300

    ch6 assets

--- books/bookstore_project/settings.py
+++ books/bookstore_project/settings.py
@@ -39,9 +39,18 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+
+    # Third-party
+    'crispy_forms', # new
+
+
     'users.apps.UsersConfig',
     'pages.apps.PagesConfig',
 ]
+
+# django-crispy-forms
+CRISPY_TEMPLATE_PACK = 'bootstrap4' # new
+
 
 
 MIDDLEWARE = [
@@ -127,6 +136,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_FINDERS = [
+    "django.contrib.staticfiles.finders.FileSystemFinder",
+    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- books/pages/views.py
+++ books/pages/views.py
@@ -1,8 +1,7 @@
-from django.shortcuts import render
 from django.views.generic import TemplateView
 
 class HomePageView(TemplateView):
     template_name = 'home.html'
 
-def render_home_page(request):
-    return render(request, 'home.html')
+class AboutPageView(TemplateView):
+    template_name = 'about.html'
